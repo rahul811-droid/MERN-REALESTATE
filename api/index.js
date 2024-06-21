72402222b5f95869f7283a012c4906e712d8ab61
unknown <rahul25062@gmail.com>
2024-06-21 21:59:35 +0530

    message

--- api/index.js
+++ api/index.js
@@ -1,60 +1,56 @@
-
-
-import express from "express";
-import mongoose from "mongoose"
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
-import listingRouter from './routes/listings.route.js'
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listings.route.js';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';  // Import the cors package
 import path from 'path';
-dotenv.config() ;
 
+dotenv.config();
 
-
-
-
-console.log(process.env.MONGO)
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Connected to Mongodb!')
-}).catch((err)=>{
-    console.log(err)
-})
+console.log(process.env.MONGO);
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB!');
+}).catch((err) => {
+    console.log(err);
+});
 
 const __dirname = path.resolve();
-
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());  // Use the cors middleware
+
+app.use(cors({
+    origin: 'https://mern-realestate-nqv3.onrender.com', // Replace with your frontend URL
+    credentials: true // If you need to include credentials (cookies, authorization headers, etc.)
+}));
 
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000!')
-})
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!');
+});
 
-
-
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-app.use('/api/listings',listingRouter);
-
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listings', listingRouter);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
-
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal server Error ' ;
+    const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
-        success:false,
+        success: false,
         statusCode,
         message
-    })
-})
-
+    });
+});
